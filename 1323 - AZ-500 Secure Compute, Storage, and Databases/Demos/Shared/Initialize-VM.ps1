6d1a22a7f06e27b4873c89856f70e43df7f4469a
Wayne Hoggett <wayne-hoggett@pluralsight.com>
2023-10-05 09:42:46 +1000

    Create Initialize-VM.ps1

---
+++ 1323 - AZ-500 Secure Compute, Storage, and Databases/Demos/Shared/Initialize-VM.ps1
@@ -0,0 +1,50 @@
+#region Clean up Microsoft Edge
+try {
+    Write-Verbose "START: Clean up Microsoft Edge"
+    # Create the Directory Tree
+    New-Item -Path "HKLM:\Software\Policies\Microsoft\Edge\PasswordManagerEnabled" -Force
+    New-Item -Path "HKLM:\Software\Policies\Microsoft\Edge\RestoreOnStartupURLs" -Force
+    # Disable full-tab promotional content
+    Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "PromotionalTabsEnabled" -Value 0 -Type "DWord" -Force
+    # Disable Password Manager
+    Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Edge\PasswordManagerEnabled" -Name "PromotionalTabsEnabled" -Value 0 -Type "DWord" -Force
+    # Disallow importing of browser settings
+    New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Edge" -Name "ImportBrowserSettings" -Value 0 -Force
+    # Disallow Microsoft News content on the new tab page
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "NewTabPageContentEnabled" -Value 0 -Type "DWord" -Force
+    # Disallow all background types allowed for the new tab page layout
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "NewTabPageAllowedBackgroundTypes" -Value 3 -Type "DWord" -Force
+    # Hide App Launcher on Microsoft Edge new tab page
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "NewTabPageAppLauncherEnabled" -Value 0 -Type "DWord" -Force
+    # Disable the password manager
+    New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Edge" -Name "PasswordManagerEnabled" -Value '0' -Force
+    # Hide the First-run experience and splash screen
+    New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Edge" -Name "HideFirstRunExperience" -Value 1 -Force
+    # Disable sign-in
+    New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Edge" -Name "BrowserSignin" -Value 0 -Force
+    # Disable quick links on the new tab page
+    New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Edge" -Name "NewTabPageQuickLinksEnabled" -Value 0 -Force
+    # Disable importing of favorites
+    New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Edge" -Name "ImportFavorites" -Value 0 -Force
+    Write-Verbose "END: Clean up Microsoft Edge"
+}
+catch {
+    Write-Verbose "ERROR: Clean up Microsoft Edge"
+    throw $_
+}
+
+#endregion Clean up Microsoft Edge
+
+# Customize Windows Explorer
+try {
+    Write-Verbose "START: Customize Windows Explorer"
+    Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask -Verbose
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "HideClock" -Value 1
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "DisableNotificationCenter" -Value 1
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "HideSCAVolume" -Value 1
+    Write-Verbose "END: Customize Windows Explorer"
+}
+catch {
+    Write-Verbose "ERROR: Customize Windows Explorer"
+    throw $_
+}
